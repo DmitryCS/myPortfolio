76c8386986ecd494b67b9048812f561bf511f94a
Dmitry <dmitry.lyzhin2014@yandex.ru>
2019-11-20 19:53:48 +0500

    first commit

---
+++ nav_and_footer.js
@@ -0,0 +1,25 @@
+document.getElementById("insertNavMenu").innerHTML = 
+        `<nav class="menu">
+            <ul>        
+                <li><a href="main.html"><i class="fa fa-home fa-fw"></i>Who am I?</a></li>        
+                <li><a href="skills.html">Do I have skills?</a></li>        
+                <li><a href="langs.html">Do I know computer languages?</a></li>        
+                <li><a href="ican.html">What can I do?</a></li>           
+                <li><a href="exs.html">Examples of work</a></li>        
+            </ul>
+        </nav>`;
+        //document.body.innerHTML = "dasf" + document.body.innerHTML;
+        //document.getElementById("insertNavMenu").innerHTML = "WWW"+Date();
+        function myf(){
+        //    document.getElementById("load").innerHTML = "WWWw";
+        }
+        document.getElementById("insertFooter").innerHTML = `
+        <div class="footer">
+            <h1>Links to me</h1>
+            <ul>
+                <li><a href="https://vk.com/dimacs"><img src="images/vk.svg" height="100" width="100"></a></li>
+                <li><a href="https://github.com/DmitryCS"><img src="images/github.svg" height="100" width="100"></a></li>
+                <li><a href="https://codeforces.com/profile/Phantom_"><img src="images/cdf.png" height="100" width="100"></a></li>
+                <li><a href="https://stepik.org/users/82724021"><img src="images/stepik.svg" height="100" width="100"></a></li>
+            </ul>
+        </div>`;
